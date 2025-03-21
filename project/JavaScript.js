0b8644eec8e44aec1d7424ea33fa8ec43aa940f9
Nicolas Lehner <n.lehner@students.htl-leonding.ac.at>
2025-03-21 12:28:05 +0100

    sound Button

--- project/JavaScript.js
+++ project/JavaScript.js
@@ -12,4 +12,22 @@ document.addEventListener('keydown', function(event) {
     }
 });
 
+const img = document.getElementById("soundImage");
+        const sound = document.getElementById("sound");
+
+        img.addEventListener("click", () => {
+            if (sound.paused) {
+                sound.play();
+                img.src = "media/sound.png";
+            } else {
+                sound.pause();
+                sound.currentTime = 0;
+                img.src = "media/noSound.png";
+            }
+        });
+
+        sound.addEventListener("ended", () => {
+            img.src = "media/noSound.png";
+        });
+
 /* --------------------Game-------------------- */
